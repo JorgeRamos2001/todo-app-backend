1d066048f85c75931c5ad79971b4a3dcb5217541
Jorge Ramos <ramosjar2001@gmail.com>
2026-05-18 09:49:11 -0600

    feat: implement spring security config jwt filter stack and global exception handler

---
+++ src/main/java/com/todoapp/exception/ExceptionResponse.java
@@ -0,0 +1,12 @@
+package com.todoapp.exception;
+
+import java.time.LocalDateTime;
+
+public record ExceptionResponse(
+        Integer status,
+        String error,
+        Object details,
+        String path,
+        LocalDateTime timestamp
+) {
+}
